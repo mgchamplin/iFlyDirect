f19fd2541f57427878f42409287b8f909876a412
Cup of Coding <anne@cupofcoding.com>
2022-08-08 16:36:00 -0700

    added navbar

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./home.css"
+import Navbar from '../../components/navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+    <p>Home</p>
+      <Navbar />      
+    </div>
+  )
+}
+
+export default Home;
