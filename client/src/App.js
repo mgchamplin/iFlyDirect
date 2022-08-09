3e6f4981fd4388f2fafd1e2dae72d9d30671047b
Cup of Coding <anne@cupofcoding.com>
2022-08-08 17:16:24 -0700

    basic home page layout, missing functionality.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter,
   Route,
 } from 'react-router-dom';
 import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Flight from './pages/flight/Flight';
 
 // import logo from './logo.svg';
 // import './App.css';
@@ -12,6 +14,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/flights' element={<List/>} />
+        <Route path='/flights/:id' element={<Flight/>} />
+
       </Routes>
     </BrowserRouter>
 
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import "./home.css"
 import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
 
 const Home = () => {
   return (
-    <div>
-    <p>Home</p>
-      <Navbar />      
+    <div>    
+      <Navbar />
+      <Header />
     </div>
   )
 }
