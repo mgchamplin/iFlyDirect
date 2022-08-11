50bd9676628521b244adee3e77e3fbf28520c095
Paul <apvivar92@gmail.com>
2022-08-10 18:51:44 -0700

    added index.js in models to connect to db

--- admin/index.js
+++ admin/index.js
@@ -1,31 +1,32 @@
 // Modules and Globals
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const methodOverride = require('method-override')
+// require('dotenv').config()
+const db = require("./models");
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
 
 // Express Settings
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser:    true, 
-    useUnifiedTopology: true,
-  })
+// mongoose.connect(process.env.MONGODB_URI, {
+//     useNewUrlParser:    true, 
+//     useUnifiedTopology: true,
+//   })
 
-if (process.env.DEBUG==1) console.log("CONNECTING TO =" + process.env.MONGODB_URI) 
+if (process.env.DEBUG==1) console.log("CONNECTING TO =" + process.env.MONGODB_URI);
 
 
 // Controllers & Routes
-app.use('/users', require('./controllers/users'))
-app.use('/flights', require('./controllers/flights'))
+app.use('/users', require('./controllers/users'));
+app.use('/flights', require('./controllers/flights'));
 
 
 if (process.env.DEBUG==1) console.log("Listening on Port " + process.env.PORT + " (set in your .env file)")
-app.listen(process.env.PORT)              
+app.listen(process.env.PORT);
---
+++ admin/models/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser:    true, 
+    useUnifiedTopology: true,
+  });
+
+module.exports.Location = require("./location");
+module.exports.Flight = require("./flight");
