a1f68cd1d80255c2840f70b78992426cb3409d9c
Paul <apvivar92@gmail.com>
2022-08-12 19:25:52 -0700

    query all locations in db

--- admin/seeders/flight.js
+++ admin/seeders/flight.js
@@ -1,22 +1,23 @@
 // require models folder to give CRUD access to database in models
 const db = require("../models");
 
-let place = db.Location.findOne();
-
-// create a new place with create(). Pass in an array to create multiple places at once.
-db.Flight.create([
-  {
-    from: place,
-    to: place,
-    price: 300,
-    date: new Date(),
-    roundTrip: false
-  }
-  
-]).then(() => {
-    console.log("Success!");
-    process.exit(); //To close process
-  }).catch((err) => {
-    console.log("Failure!", err);
-    process.exit(); // To close process
-  });
+// create a new flight with create().
+async function airport(){
+    let place = await db.Location.findOne();
+    db.Flight.create([
+    {
+        from: place,
+        to: place,
+        price: 300,
+        date: new Date(),
+        roundTrip: false
+    }
+    ]).then(() => {
+        console.log("Success!");
+        process.exit(); //To close process
+    }).catch((err) => {
+        console.log("Failure!", err);
+        process.exit(); // To close process
+    });
+}
+airport()
