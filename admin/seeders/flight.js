82535934510ee431ffbfd967115265caf546ef76
Paul <apvivar92@gmail.com>
2022-08-12 19:48:41 -0700

    loop through locations table

--- admin/seeders/flight.js
+++ admin/seeders/flight.js
@@ -2,22 +2,25 @@
 const db = require("../models");
 
 // create a new flight with create().
-async function airport(){
-    let place = await db.Location.findOne();
-    db.Flight.create([
-    {
-        from: place,
-        to: place,
-        price: 300,
-        date: new Date(),
-        roundTrip: false
+async function airport() {
+    let allLocations = await db.Location.findOne();
+    for (let i = 0; i < allLocations.length - 1; i++){
+        db.Flight.create([
+            {
+              from: allLocations[i],
+              to: allLocations[i+1],
+              price: 300,
+              date: new Date(),
+              roundTrip: false
+            }
+            
+          ]).then(() => {
+              console.log("Success!");
+              process.exit(); //To close process
+            }).catch((err) => {
+              console.log("Failure!", err);
+              process.exit(); // To close process
+            });
     }
-    ]).then(() => {
-        console.log("Success!");
-        process.exit(); //To close process
-    }).catch((err) => {
-        console.log("Failure!", err);
-        process.exit(); // To close process
-    });
-}
+};
 airport()
