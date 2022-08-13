102da727349ed2d87e41c8d5607485891ed652e5
Paul <apvivar92@gmail.com>
2022-08-12 20:06:57 -0700

    to and from airport logic

--- admin/seeders/flight.js
+++ admin/seeders/flight.js
@@ -1,9 +1,10 @@
 // require models folder to give CRUD access to database in models
 const db = require("../models");
 
+let allLocations = await db.Location.find();
+
 // create a new flight with create().
-async function airport() {
-    let allLocations = await db.Location.findOne();
+async function toAirport() {
     for (let i = 0; i < allLocations.length - 1; i++){
         db.Flight.create([
             {
@@ -23,4 +24,27 @@ async function airport() {
             });
     }
 };
-airport()
+toAirport();
+
+//reverse for from airport
+async function fromAirport() {
+    for (let i = allLocations.length; i != 0; i--){
+        db.Flight.create([
+            {
+              from: allLocations[i],
+              to: allLocations[i-1],
+              price: 300,
+              date: new Date(),
+              roundTrip: false
+            }
+            
+          ]).then(() => {
+              console.log("Success!");
+              process.exit(); //To close process
+            }).catch((err) => {
+              console.log("Failure!", err);
+              process.exit(); // To close process
+            });
+    }
+};
+fromAirport();
