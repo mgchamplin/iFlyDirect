a650e88ab2895187640d767ec19ecf4b236c5cf4
Michael Champlin <mchamplin@sbcglobal.net>
2022-08-16 12:01:05 -0700

    Added first/last name and email address to users model

--- admin/models/index.js
+++ admin/models/index.js
@@ -1,13 +1,5 @@
 require("dotenv").config();
 
-const mongoose = require("mongoose");
-
-console.log("MongoDB_URI is " + process.env.MONGODB_URI);
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser:    true, 
-    useUnifiedTopology: true,
-  });
 module.exports.Location = require("./location");
 module.exports.Flight = require("./flight");
 module.exports.User = require('./user');
