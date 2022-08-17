1eb638f70704892f9335f8bc81f818abfafe9ee5
Michael Champlin <mchamplin@sbcglobal.net>
2022-08-17 12:20:35 -0700

    add console.log for crash debug

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 // import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
+console.log("Booting front end 1")
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
